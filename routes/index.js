b7f528abd786a33d50b02091f7acb84425f2d3e5
KimChanaDev <chanaweestr@gmail.com>
2023-01-15 17:20:17 +0700

    cancel enSureAuthenticated, show class table

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,19 @@ const express = require('express')
 const router = express.Router()
 const classesModel = require('../model/classes')
 
-// enSureAuthenticated
-// -------------------------------
-router.get('/', enSureAuthenticated, (req,res)=>{
+router.get('/', (req,res)=>{
     classesModel.getClasses((err,classes)=>{
         res.render('index.ejs', { classes:classes })
     })
 })
 
-function enSureAuthenticated(req, res, next){
-    if(req.isAuthenticated()){
-        return next()
-    }
-    else{
-        res.redirect('/user/login')
-    }
-  }
-// -------------------------------
+// function enSureAuthenticated(req, res, next){
+//     if(req.isAuthenticated()){
+//         return next()
+//     }
+//     else{
+//         res.redirect('/user/login')
+//     }
+// }
 
 module.exports = router
