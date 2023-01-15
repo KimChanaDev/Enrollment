e13f244daecfe7fd42e2a2012b497dd7d873b438
KimChanaDev <chanaweestr@gmail.com>
2023-01-15 17:49:58 +0700

    add lesson button, save lesson in classes model

--- model/classes.js
+++ model/classes.js
@@ -9,15 +9,31 @@ const classesSchema = mongoose.Schema({
     title:String,
     description:String,
     instructor:String,
-
-    class_id:String
+    class_id:String,
+    
+    lesson:[{
+        lesson_number: Number,
+        lesson_title: String,
+        lesson_body: String
+    }]
 })
 const classesModel = mongoose.model('classes', classesSchema)
 module.exports = classesModel
 module.exports.getClasses = function(callback, limit){
     classesModel.find(callback).limit(limit)
 }
-
 module.exports.saveNewClass = function(newClass, callback){
     newClass.save(callback)
+}
+module.exports.addLesson = (info, callback)=>{
+    class_id = info['class_id']
+    lesson_number = info['lesson_number']
+    lesson_title = info['lesson_title']
+    lesson_body = info['lesson_body']
+
+    const query = {class_id: class_id}
+    classesModel.findOneAndUpdate(query, {$push:{"lesson":{ lesson_number:lesson_number,
+                                                            lesson_title: lesson_title,
+                                                            lesson_body: lesson_body }}}
+                                        , { save:true, upsert:true }, callback)
 }
--- routes/instructors.js
+++ routes/instructors.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const router = express.Router()
 const instructorModel = require('../model/instructors')
+const classesModel = require('../model/classes')
 
 router.get('/classes', (req,res)=>{
     instructorModel.getInstructorByUsername(req.user.username ,(err, instructor)=>{
         res.render('instructors/add_classes.ejs', {instructor: instructor})
     })
+})
+
+router.get('/classes/:id/lesson/add', (req, res)=>{
+    res.render('instructors/newlesson.ejs', { class_id: req.params.id })
+})
+router.post('/classes/:id/lesson/add', (req, res)=>{
+    let info = []
+    info['class_id'] = req.params.id
+    info['lesson_number'] = req.body.lesson_number
+    info['lesson_title'] = req.body.lesson_title
+    info['lesson_body'] = req.body.lesson_body
+
+    classesModel.addLesson(info, (err, losson)=>{
+        if(err){
+            console.log(err)
+            throw err
+        }
+    })
+    res.redirect('/instructor/classes')
 })
 
 
